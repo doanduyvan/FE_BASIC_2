91d83bc3c242162475113fb89a2cabe2f5f285c5
doanduyvan <duyvanlee2001@gmail.com>
2024-01-19 23:06:54 +0700

    up

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,7 +1,18 @@
 
 
 window.addEventListener('load',()=>{
-    let width = document.documentElement.clientWidth;
+   in_width1();
+})
+
+window.addEventListener('resize',()=>{
+   in_width1();
+})
+
+
+
+
+function in_width1(){
+     let width = document.documentElement.clientWidth;
     const in_width = document.querySelector('.in-width');
     in_width.textContent = "Width của trình duyệt là: " + width;
-})
+}
